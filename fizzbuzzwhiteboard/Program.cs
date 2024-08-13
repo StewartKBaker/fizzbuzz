f6f8e40c6a71c69043475f29697ddb0fac94e247
StewartBaker <bakrstewart16@gmail.com>
2024-08-13 10:40:18 -0500

    finished whiteboard

---
+++ fizzbuzzwhiteboard/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace fizzbuzzwhiteboard;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        static string FizzBuzz(int x)
+        {
+            var fizzbuzz = "";
+            if (x % 3 == 0)
+            {
+                fizzbuzz += "fizz";
+            }
+
+            if (x % 5 == 0)
+            {
+                fizzbuzz += "buzz";
+            }
+
+            return fizzbuzz;
+        }
+        
+        Console.WriteLine(1);
+        Console.WriteLine(FizzBuzz(3));
+        Console.WriteLine(FizzBuzz(5));
+        Console.WriteLine(FizzBuzz(15));
+    }
+}
